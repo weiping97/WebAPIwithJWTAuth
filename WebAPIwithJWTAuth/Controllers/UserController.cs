0cff5940196a737af8d11de1b45432d2997b6bfa
wei ping <weipingyap97@gmail.com>
2021-03-11 21:47:52 +0800

    Trying out JWT Authentication

---
+++ WebAPIwithJWTAuth/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIwithJWTAuth.JWTSetting;
+
+namespace WebAPIwithJWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly JWTSettings _jwtsettings;
+
+        public UserController(IOptions<JWTSettings> jwtsettings)
+        {
+            _jwtsettings = jwtsettings.Value;
+        }
+        public IActionResult Login()
+        {
+            string emailAddress = "";
+            //sign your token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtsettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, emailAddress)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var userToken = tokenHandler.WriteToken(token);
+
+            return Ok(userToken);
+        }
+    }
+}
